90cc4f6362de14c58f6c60dd4cb060a39a24604f
Sanika Mogal <sanikamogal925@gmail.com>
2026-01-11 23:03:57 +0530

    gsap timrline

---
+++ 03_GSAP_Timeline/script.js
@@ -0,0 +1,20 @@
+gsap.to("#box1",{
+    x:1200,
+    duration:1.5,
+    delay:1,
+    rotate:360
+})
+gsap.to("#box2",{
+    x:1200,
+    backgroundColor:"yellow",
+    borderRadius:50,
+    duration:2,
+    delay:2.5, //(1.5+1 of box 1)
+})
+gsap.to("#box3",{
+    x:1200,
+    scale:0.5,
+    borderRadius:"50%",
+    duration:1.5,
+    delay:4,
+})
