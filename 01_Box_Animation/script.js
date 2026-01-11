1037c16c7003982ef54d6612b214fb590b6766d4
Sanika Mogal <sanikamogal925@gmail.com>
2026-01-11 22:53:59 +0530

    First Commit

--- 01_Box_Animation/script.js
+++ 01_Box_Animation/script.js
@@ -21,21 +21,40 @@
 //     delay:1
 // })
 
+// gsap.to("#box1",{
+//     x:1200,
+//     duration:2,
+//     delay:1,
+//     rotate: 360,
+//     backgroundColor: "#cf8796ff",
+//     borderRadius: "50%",
+//     scale:2,
+// })
+// gsap.from("#box2",{
+//     x:1200,
+//     duration:2,
+//     delay:1,
+//     rotate: 360,
+//     backgroundColor: "#401fe4ff",
+//     borderRadius: "50%",
+//     scale:2,
+// })
+
+// Repeat the animation in single direction
 gsap.to("#box1",{
     x:1200,
     duration:2,
     delay:1,
-    rotate: 360,
-    backgroundColor: "#cf8796ff",
-    borderRadius: "50%",
-    scale:2,
+    rotate:360,
+    repeat:1,// Repeat the animation 
+    repeat:-1, //repeat infinitely
 })
-gsap.from("#box2",{
+// Repeat the animation in single direction
+gsap.to("#box2",{
     x:1200,
     duration:2,
     delay:1,
-    rotate: 360,
-    backgroundColor: "#401fe4ff",
-    borderRadius: "50%",
-    scale:2,
+    rotate:360,
+    repeat:-1, //repeat infinitely
+    yoyo:true
 })
--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 ### GSAP (GreenSock Animation Platform)
 
-- powerful JavaScript animation library used to create high-performance, smooth animations for websites and web apps.
-
+Powerful JavaScript animation library used to create high-performance, smooth animations for websites and web apps.
 --------------------------------------------------
+
 ## gsap.to()
 
 👉 Animates an element from its current state to a new state.
-
 --------------------------------------------------
 
 ## gsap.from()
 
 👉 Animates an element from a given state to its current state.
+--------------
 
+## yoyo
+
+yoyo makes an animation reverse back to its starting state after it finishes—like a yo-yo going forward and coming back
+----------------------------
+
+## stagger
+
+stagger is used when you want to animate multiple elements one after another instead of all at once, giving a smooth, flowing effect.
